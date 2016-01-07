aa4733de8092fdc7f611c57a28e846a2b410b0e5
Arnoud Witt <awitt@ebay.com>
2016-01-07 16:59:17 +0100

    Revert "Removed `:use_hierarchy` functionality"
    This reverts commit cf9b1600fe3537dbbda0858452149f7d396affa5.
    
    Conflicts:
    	README.md : fixed

--- lib/hiera/backend/vault_backend.rb
+++ lib/hiera/backend/vault_backend.rb
@@ -10,6 +10,7 @@ class Hiera
         @config = Config[:vault]
         @config[:mounts] ||= {}
         @config[:mounts][:generic] ||= ['secret']
+        @config[:use_hierarchy] ||= 'no'
         @config[:default_field_parse] ||= 'string' # valid values: 'string', 'json'
 
         if not ['string','json'].include?(@config[:default_field_parse])
@@ -56,29 +57,41 @@ class Hiera
         # Only generic mounts supported so far
         @config[:mounts][:generic].each do |mount|
           path = Backend.parse_string(mount, scope, { 'key' => key })
-          Hiera.debug("Looking in path #{path}")
-          new_answer = lookup_generic("#{path}/#{key}", scope)
-          #Hiera.debug("[hiera-vault] Answer: #{new_answer}:#{new_answer.class}")
-          next if new_answer.nil?
-          case resolution_type
-          when :array
-            raise Exception, "Hiera type mismatch: expected Array and got #{new_answer.class}" unless new_answer.kind_of? Array or new_answer.kind_of? String
-            answer ||= []
-            answer << new_answer
-          when :hash
-            raise Exception, "Hiera type mismatch: expected Hash and got #{new_answer.class}" unless new_answer.kind_of? Hash
-            answer ||= {}
-            answer = Backend.merge_answer(new_answer,answer)
-          else
-            answer = new_answer
-            found = true
-            break
+          datasources(scope, order_override) do |source|
+            Hiera.debug("Looking in path #{path}#{source}")
+            new_answer = lookup_generic("#{path}#{source}#{key}", scope)
+            #Hiera.debug("[hiera-vault] Answer: #{new_answer}:#{new_answer.class}")
+            next if new_answer.nil?
+            case resolution_type
+            when :array
+              raise Exception, "Hiera type mismatch: expected Array and got #{new_answer.class}" unless new_answer.kind_of? Array or new_answer.kind_of? String
+              answer ||= []
+              answer << new_answer
+            when :hash
+              raise Exception, "Hiera type mismatch: expected Hash and got #{new_answer.class}" unless new_answer.kind_of? Hash
+              answer ||= {}
+              answer = Backend.merge_answer(new_answer,answer)
+            else
+              answer = new_answer
+              found = true
+              break
+            end
           end
 
           break if found
         end
 
         return answer
+      end
+
+      def datasources(scope, order_override)
+        if @config[:use_hierarchy] == 'yes'
+          Backend.datasources(scope, order_override) do |source|
+            yield("/#{source}/")
+          end
+        else
+          yield("/")
+        end
       end
 
       def lookup_generic(key, scope)
